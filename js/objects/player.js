5295d2f1ff90b3a630eef4b15049678b3527db1e
gion <oancaionutandrei@gmail.com>
2018-07-29 14:27:55 +0200

    Morphing spheres into cool snowmen

--- js/objects/player.js
+++ js/objects/player.js
@@ -10,12 +10,33 @@ function Player(scene, world) {
 }
 
 Player.prototype.createModel = function() {
-  // Create mesh
-  var geometry = new THREE.SphereGeometry(this.radius, 32, 32 );
-  var material = new THREE.MeshLambertMaterial({color: 0xeeee00});
-  this.mesh = new THREE.Mesh(geometry, material);
-  this.mesh.castShadow = true;
-  this.scene.add(this.mesh);
+  var that = this;
+  // Load material
+  var mtlLoader = new THREE.MTLLoader();
+  mtlLoader.setPath('assets/models/');
+  mtlLoader.load('Snowman.mtl', function(materials) {
+    materials.preload();
+    // Create model
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setPath('assets/models/');
+    objLoader.setMaterials(materials);
+    // Load model
+    objLoader.load('Snowman.obj', function(object) {
+        that.mesh = object;
+        that.mesh.traverse(function(child) {
+          child.castShadow = true;
+        });
+        object.scale.set(0.05, 0.05, 0.05);
+        that.scene.add(object);
+      },
+      function(xhr) {
+        console.log((xhr.loaded / xhr.total * 100 ) + '% loaded');
+      },
+      function(error) {
+        console.log('An error happened:', error);
+      }
+    );
+  });
 }
 
 Player.prototype.createLight = function() {
@@ -37,6 +58,7 @@ Player.prototype.createBody = function() {
     position: new CANNON.Vec3(0, 0, 0),
     shape: new CANNON.Sphere(this.radius)
   });
+  this.body.linearDamping = 0.1;
   this.world.addBody(this.body);
 }
 
@@ -45,6 +67,7 @@ Player.prototype.setPosition = function(x, y, z) {
 }
 
 Player.prototype.getPosition = function() {
+  if (!this.mesh) return {x: 0, y: 0, z: 0};
   return this.mesh.position;
   // If you're wondering why I haven't used the body's position
   // it's because it won't work, for unknown reasons
@@ -83,12 +106,30 @@ Player.prototype.jump = function() {
   this.body.applyImpulse(new CANNON.Vec3(0, 0, 1), this.body.position);
 }
 
+Player.prototype.getMeshRotation = function() {
+  var currentDirection = vec3.fromValues(this.body.velocity.x, this.body.velocity.y, 0);
+  var meshDirection = vec3.fromValues(1, 0, 0);
+  var angle = vec3.angle(currentDirection, meshDirection);
+
+  // Fix sign
+  var cross = vec3.create();
+  var plane = vec3.fromValues(0, 0, 1);
+  vec3.cross(cross, meshDirection, currentDirection);
+  var sign = vec3.dot(plane, cross);
+  angle *= Math.sign(sign);
+
+  return angle;
+}
+
 Player.prototype.update = function() {
   // Update mesh position
   var position = this.body.position;
-  this.mesh.position.x = position.x;
-  this.mesh.position.y = position.y;
-  this.mesh.position.z = position.z;
+  if (this.mesh) {
+    this.mesh.position.x = position.x;
+    this.mesh.position.y = position.y;
+    this.mesh.position.z = position.z - this.radius;
+    this.mesh.rotation.z = this.getMeshRotation();
+  }
 
   this.light.position.x = position.x;
   this.light.position.y = position.y;
